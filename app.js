0119ed5cda2a9500ddf2028926f42e173cb1eb82
Danilo Mezgec <danilo@itech.media>
2016-08-09 13:40:27 +0100

    Some ideas added

---
+++ app.js
@@ -0,0 +1,8 @@
+// Retrieve the viewport width and build up conditionals for enable/disable or switch between effects
+
+// Retrieve the target element height and cut it with a blur effect at the set height, depending of the viewport width and device orientation
+
+// Set an event that trigger the show action, like when you scroll to the target element
+
+
+
